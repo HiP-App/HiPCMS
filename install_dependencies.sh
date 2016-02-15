4522978b4a52f77309ddb23585097287ab7182ac
Jan Lippert <lippertsjan@gmail.com>
2016-02-15 22:09:53 +0100

    Trying something else

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT=$(pwd)
 nvm install stable
 nvm use stable
 cd $PROJECT_ROOT/client
-npm install bower grunt-cli
+npm install
 ./node_modules/bower/bin/bower install
 cd $PROJECT_ROOT/server
 sbt update
