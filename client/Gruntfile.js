87a0c1832a78a52c10a4c9edd0db3ed7d519d610
Jan Lippert <lippertsjan@gmail.com>
2016-02-17 19:06:55 +0100

    Disabled protractor

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -506,7 +506,8 @@ module.exports = function (grunt) {
     'shell:xvfb',     //*
     'env:xvfb',       //*
     'karma',
-    'protractor:run',
+    // 'protractor:run',
+    // currently disabled for travis
     'shell:xvfb:kill' //*
   ]);
 
