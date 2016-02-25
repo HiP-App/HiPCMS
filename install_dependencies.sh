40cd715b8104c683d9e609397803e491d10aca82
Jan Lippert <lippertsjan@gmail.com>
2016-02-25 23:40:05 +0100

    Adapted installation script to work outside Travis CI
    HIPCMS-298 #time 45m

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
 PROJECT_ROOT=$(pwd)
-. $HOME/.nvm/nvm.sh
-nvm install stable
-nvm use stable
+
+#####################################################################
+#
+# Preparations: checking if some necessary programs are installed
+#
+#####################################################################
+
+travisSetup(){
+  if [ hash ". $HOME/.nvm/nvm.sh" 2>/dev/null ]; then
+    . $HOME/.nvm/nvm.sh || return 1
+    nvm install stable  || return 1
+    nvm use stable      || return 1
+  fi
+}
+
+travisSetup || echo >&2 "Travis setup failed. Installation will try to continue without nvm."
+
+hash npm 2>/dev/null || { echo >&2 "npm is not installed. Installation cannot continue."; exit 1; }
+hash sbt 2>/dev/null || { echo >&2 "sbt is not installed. Installation cannot continue."; exit 1; }
+
+#####################################################################
+#
+# Actual Installation
+#
+#####################################################################
+
+
 cd $PROJECT_ROOT/client
+echo "npm install:"
 npm install
+echo "npm install bower:"
+npm install bower
+echo "bower install:"
 ./node_modules/bower/bin/bower install
+echo "update webdriver:"
 ./node_modules/protractor/bin/webdriver-manager update
 cd $PROJECT_ROOT/server
 sbt update
