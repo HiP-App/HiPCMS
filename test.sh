4c0a8fda764f29056c337929a6605fa34fdd50e2
Jan Lippert <lippertsjan@gmail.com>
2016-03-16 22:40:40 +0100

    Added function exit_on_error

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sbt clean coverage test
-sbt coverageReport
-sbt coverageAggregate
-sbt codacyCoverage
+function exit_on_error {
+  echo "Step \"$1\" of test.sh failed"
+  exit 1
+}
+
+
+sbt clean coverage test  || exit_on_error "sbt clean coverage test"
+sbt coverageReport       || exit_on_error "sbt coverageReport"
+sbt coverageAggregate    || exit_on_error "sbt coverageAggreagate"
+sbt codacyCoverage       || exit_on_error "sbt codacyCoverage"
